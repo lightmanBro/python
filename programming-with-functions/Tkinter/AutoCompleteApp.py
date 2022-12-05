a691091173f096a1b11c5ba34f89d8df0a350a25
LightmanBro <omotosodavidromeo@gmail.com>
2022-12-05 17:06:33 +0100

    ...

--- programming-with-functions/Tkinter/AutoCompleteApp.py
+++ programming-with-functions/Tkinter/AutoCompleteApp.py
@@ -5,14 +5,17 @@ root = Tk()
 mylabel = Label(root, text='HELLO WORLD').grid(row=1, column=0)
 mylabel1 = Label(root, text='WORD CORRECTION / AUTOCOMPLETE APP').grid(row=0, column=0)
 
+# Getting user Input
 user_inpt = Entry(root, width=35, borderwidth=5)
+# Design for the User input Box
 user_inpt.grid(row=1, column=0, padx=20)
 
+# Getting the user input and printing it on the screen
 def myclick():
     mylabel = Label(root, text= user_inpt.get())
     mylabel.grid(row=3, column=0)
 
-
+# Creating a button and designing the interface
 myButton = Button(root, text='Check Word', padx=10, command=myclick, background='black', fg='white').grid(row=2, column=0)
 # Positioning it on the screen
 root.mainloop() 
